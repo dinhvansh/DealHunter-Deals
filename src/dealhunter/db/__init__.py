2155fb5e38cc6800b90b87e7e3329d6053b6cd46
dinhvansh <vanqn95@gmail.com>
2026-09-09 15:30:33 +0700

    chore: export extended persistence models

--- src/dealhunter/db/__init__.py
+++ src/dealhunter/db/__init__.py
@@ -1,4 +1,28 @@
 from .base import Base
+from .extended_models import (
+    AffiliateClick,
+    AffiliateLinkRecord,
+    AlertRecord,
+    DealScoreRecord,
+    PromotionEvaluation,
+    VerificationRun,
+    Voucher,
+    WatchlistRecord,
+)
 from .models import Listing, PriceSnapshot, ProductVariant, Shop
 
-__all__ = ["Base", "Shop", "Listing", "ProductVariant", "PriceSnapshot"]
+__all__ = [
+    "Base",
+    "Shop",
+    "Listing",
+    "ProductVariant",
+    "PriceSnapshot",
+    "Voucher",
+    "PromotionEvaluation",
+    "DealScoreRecord",
+    "WatchlistRecord",
+    "VerificationRun",
+    "AffiliateLinkRecord",
+    "AffiliateClick",
+    "AlertRecord",
+]
